b75ee4d0cd83ee24f24a094b1ebdf1010e8e8c18
vodinhhuy0911 <vohuy9112000@gmail.com>
2021-08-14 22:00:30 +0700

    update link

--- Data/data/GetLink2.py
+++ Data/data/GetLink2.py
@@ -51,9 +51,9 @@ def getLinkDay(day,path):
 
 
 getLinkDay("","linkRead.txt")
-getLinkDay("2021/08/11","linkReadDay.txt")
-getLinkDay("2021/aug/11","linkReadDay1.txt")
-getLinkDay("2021-08-11","linkReadDay2.txt")
-getLinkDay("20210811","linkReadDay3.txt")
+getLinkDay("2021/08/13","linkReadDay.txt")
+getLinkDay("2021/aug/13","linkReadDay1.txt")
+getLinkDay("2021-08-13","linkReadDay2.txt")
+getLinkDay("20210813","linkReadDay3.txt")
 getLinkDay("html","linkReadHtml.txt")
 
